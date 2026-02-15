c60592c2025b7953090a77127c9da0361095a295
balinder <balinder@localhost>
2026-02-15 15:20:36 +0530

    Eureka Server Microservice

---
+++ Service-Discovery/src/test/java/com/balinder/service/discovery/Service/Discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balinder.service.discovery.Service.Discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
